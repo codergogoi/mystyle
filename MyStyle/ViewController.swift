a244e31a9545a727fcd2ba4c2a69a4f557204025
CoderGogoi <codergogoi@gmail.com>
2017-09-17 14:28:43 +0530

    Initial Commit

---
+++ MyStyle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyStyle
+//
+//  Created by Jayanta Gogoi on 17/09/17.
+//  Copyright © 2017 Jayanta Gogoi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
